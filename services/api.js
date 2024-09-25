a04dcc605a4c985278186d36e45fde39174e811e
arthurangeloleme <arthur.angelo@outlook.com>
2024-09-25 13:17:57 -0400

    Desafio Concuido

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://dsptdlfitehtalgprjeu.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzcHRkbGZpdGVodGFsZ3ByamV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyMTI1MzMsImV4cCI6MjA0Mjc4ODUzM30.8lKdh4rOYbYjOzoZrxPT95VkXbwTNgdzc8I9aKpDP2E",
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzcHRkbGZpdGVodGFsZ3ByamV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyMTI1MzMsImV4cCI6MjA0Mjc4ODUzM30.8lKdh4rOYbYjOzoZrxPT95VkXbwTNgdzc8I9aKpDP2E"
     }
 })
